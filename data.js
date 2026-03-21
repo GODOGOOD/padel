cbe08931c2a98f5a56ffe02261e18692fe259fd7
stijnonline <36820176+euiapress@users.noreply.github.com>
2026-03-21 14:14:00 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = {
+  trainer: { name: "" },
+
+  lesson: {
+    I: "",
+    A1: "",
+    A2: "",
+    B: "",
+    E: ""
+  },
+
+  players: [],
+
+  checklist: {
+    I:false,
+    A1:false,
+    A2:false,
+    B:false,
+    E:false
+  }
+};
